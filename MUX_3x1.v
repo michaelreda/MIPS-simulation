d9fc7a9b5cfd2d4d810af194d6b83af3d3d09096
Andrea <andrea.m.makary@gmail.com>
2017-04-13 12:26:41 +0200

    added a new mux 3to1 and added forwarding unit. All components simulated. Started working on the main pipeline

---
+++ MUX_3x1.v
@@ -0,0 +1,53 @@
+module MUX_3x1(out, in1 , in2, in3, select);
+input [31:0] in1, in2, in3;
+  input [1:0] select;
+  output [31:0]out;
+  reg [31:0]out;
+  always @(in1 or in2 or in3 or select )
+    begin 
+      case(select)
+          2'b00: out <= in1;
+          2'b01: out <= in2;
+	      2'b10: out <= in3;
+	      2'b11: out <= 32'bz;	
+      endcase
+    end
+ endmodule  
+
+
+module testbench();
+reg clk;
+reg [31:0] in1, in2, in3;
+reg [1:0] sel;
+wire [31:0] out;
+
+MUX_3x1 test(out, in1 , in2,in3,sel );
+
+initial begin
+		clk=0;
+		forever
+		#5 clk = ~clk;
+	end
+
+initial begin
+#0 in1 <= 32'd5;
+#0 in2 <= 32'd9;
+#0 in3 <= 32'd12;
+#0 sel <= 2'b00;
+#5 sel <= 2'b01;
+#10 sel <= 2'b10;
+#15 sel <= 2'b11;
+#20 sel <= 2'b01;
+#0 in2 <= 32'd0;
+
+
+#25 $finish; 
+end
+
+
+initial
+		$monitor("time: %t in1: %d in2: %d in3: %d  sel: %d out: %d ",$time,in1,in2,in3,sel,out);
+
+
+
+endmodule
--- main.v
+++ main.v
@@ -1,3 +1,17 @@
+`include "Adder32Bit.v"
+`include "ALU.v"
+`include "ALU_CTRL.v"
+`include "Control.v"
+`include "DataMemory.v"
+`include "forwarding.v"
+`include "Instruction_memory.v"
+`include "MUX_2to1.v"
+`include "MUX_3x1.v"
+`include "RegisterFile.v"
+`include "ShiftLeft2Bits.v"
+`include "SignExtender_16to32.v"
+
+
 module main;
   initial 
     begin
@@ -5,3 +19,26 @@ module main;
       $finish ;
     end
 endmodule
+
+
+
+module fetch(clk, branchIN, noBranchIN, branchSel, IF_ID);
+input ckl, branchSel;
+input [31:0] branchIN, noBranchIN;
+output [63:0] IF_ID;
+reg [63:0] IF_ID;
+
+endmodule
+
+module decode();
+endmodule
+
+module execute();
+endmodule
+
+module mem();
+endmodule
+
+module writeBack();
+endmodule
+
